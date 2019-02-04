be93347b11cfe6d4ae567ca4edce034809bfc372
dfrancoce <dfrancoce@protonmail.com>
2019-02-04 22:22:51 +0100

    Project initialization

---
+++ src/main/java/com/dfc/usersitemsui/UsersItemsUiApplication.java
@@ -0,0 +1,14 @@
+package com.dfc.usersitemsui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersItemsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersItemsUiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dfc/usersitemsui/UsersItemsUiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dfc.usersitemsui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersItemsUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
